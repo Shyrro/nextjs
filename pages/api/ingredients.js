2c210ea981824dc0adf2c214d043fb5bab235315
Shyrro <zsahmane@gmail.com>
2020-10-21 00:05:51 +0200

    feat/add put request for ingredient

--- pages/api/ingredients.js
+++ pages/api/ingredients.js
@@ -1,19 +1,35 @@
 import nextConnect from "next-connect";
 import middleware from "../../middleware/database";
+var { nanoid } = require("nanoid");
 
 const handler = nextConnect();
-
+const collectionName = "Ingredients";
 handler.use(middleware);
 
 handler.get(async (req, res) => {
   await req.db
-    .collection("Ingredients")
+    .collection(collectionName)
     .find({})
     .toArray(function (err, ingredients) {
       if (err) throw err;
-      
+
       res.send(ingredients);
     });
+});
+
+handler.put(async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const content = req.body;
+
+  if (!content) return res.status(400).send("You must write something");
+
+  const ingredient = {
+    _id: nanoid(),
+    ...content,
+  };
+
+  await req.db.collection(collectionName).insertOne(ingredient);
+  return res.send(ingredient);
 });
 
 export default handler;
